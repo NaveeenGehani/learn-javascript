e5a5f456b762dce3d1f6c948dc6528655ae6abdf
Naveen <naveengehani999@gmail.com>
2026-06-15 00:16:38 +0500

    Add datatypes notes and change name of variable notes

---
+++ basics-01/datatypes-02.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")  // this is used for Web Browser in JS
+
+// Not necessary to use semicolon ( ; ) in JS after every statement, it will not cause any issue in running the code
+
+// focus on code readability practices --> use extension like Prettier to fix this issue
+
+// JS documentation --> MDN Mozilla JS
+
+
+// Data Types
+
+let name = "hitesh"    // string => ""
+let age = 18           // number => 2 & also there is bigint => for big number like 2^53
+let isLoggedIn = false // Boolean => true/false
+// null => standalone value
+let something;         // undefinded    
+// symbol => for uniqueness
+
+// object
+
+console.log(typeof "Hola") // typeof outputs the type of Data
+
+console.log(typeof null)  // null is Object datatype
+
+console.log(typeof undefined) // undefined is undefined datatype
